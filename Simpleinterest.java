41c52de7eb3eb112e14e4a22bd4a5ae27dc53ace
Daminish <daminishukla04@yahoo.co.in>
2017-12-11 15:45:52 +0530

    new file added

---
+++ Simpleinterest.java
@@ -0,0 +1,10 @@
+public class Simpleinterest {
+	public static void main (String args [])
+	{
+		int p = 12,
+		    t = 10,
+			r = 5;
+			
+	System.out.println ("Interest amount = "+(p*r*t)/100);			
+	}
+}
