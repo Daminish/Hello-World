b72fdd65bb415b66587fdff66fa9dd56a54d1409
Daminish <daminishukla04@yahoo.co.in>
2017-12-20 22:40:01 +0530

    files added to the new branch

---
+++ HelloB1.java
@@ -0,0 +1,10 @@
+public class HelloB1 {
+	public static void main (String args [])
+	{
+		int P = 12,
+		    T = 10,
+			R = 5;
+			
+	System.out.println ("Interest amount = "+(P*R*T)/100);			
+	}
+}
